f7a97bf8e93bf4d4af9acde1a15d8dc0ebd1ff59
Leandro Duarte <tecnologia.duarte@gmail.com>
2023-04-12 17:50:04 -0300

    Erro_corrigido

--- services/calculator_test.go
+++ services/calculator_test.go
@@ -2,10 +2,12 @@ package services_test
 
 import (
 	"testing"
+
+	"github.com/Testes/services"
 )
 
 func TestSum(t *testing.T) {
-	if (services.sum(2, 2)) != 4 {
+	if (services.Sum(2, 2)) != 4 {
 		t.Error("Expected 4")
 	}
 }
